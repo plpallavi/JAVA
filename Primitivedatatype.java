e67c7d4b47358ec788a8a9088b56047f45bd0044
plpallavi <pallavi121202@gmail.com>
2022-07-22 21:20:44 +0530

    JAVA

---
+++ Primitivedatatype.java
@@ -0,0 +1,13 @@
+
+
+public class Primitivedatatype {
+    public static void main(String[] args){
+        int rollno = 64;
+        char letter = 'r';
+        float marks = 98.64f;
+        double largeDecimalNumbers = 43456247.789;
+        long largeInteger = 3456780923456l;
+        boolean check = false;        
+}
+    
+}
