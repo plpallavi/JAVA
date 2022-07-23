d4e62101708a76f7725df0bc1f797de4e1ba99b4
plpallavi <pallavi121202@gmail.com>
2022-07-23 23:01:13 +0530

    JAVA

--- Inputs.java
+++ Inputs.java
@@ -4,9 +4,24 @@ public class Inputs {
 
     public static void main(String[] args){
         Scanner input = new Scanner(System.in);
+/* 
+        //Inputs in Integers
         System.out.println("Enter your roll no ");
         int rollno = input.nextInt();
     System.out.println("Your roll no. is " + rollno);  
     
+    // Literals and Identifiers
+    int a = 234_000_000;
+    System.out.println(a);
+
+    // Inputs in Strings
+    String name = input.next();
+    //String name = input.nextline();
+    System.out.println(name);
+
+    //Inputs in Float
+*/
+    float marks = input.nextFloat();
+    System.out.println(marks);
 }
 }
