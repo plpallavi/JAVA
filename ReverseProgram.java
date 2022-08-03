c1c16548ce0b74b96b03ed4a77ee864af8fa99fa
plpallavi <pallavi121202@gmail.com>
2022-08-03 23:47:44 +0530

    JAVA

--- ReverseProgram.java
+++ ReverseProgram.java
@@ -2,6 +2,17 @@
 import java.util.Scanner;
 public class ReverseProgram {
     public static void main (String ags[]){
+        int num = 2486359;
+
+        int ans = 0;
+
+        while (num > 0){
+            int rem = num % 10;
+            num /= 10;
+
+            ans = ans + 10 + rem;
+        }
+        System.out.println(ans);
 
     }
     
