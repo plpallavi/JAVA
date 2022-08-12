26ed4f23daae2ebe4696158cb314c791f3c380e6
plpallavi <pallavi121202@gmail.com>
2022-08-12 21:44:26 +0530

    JAVA

--- StringReturn.java
+++ StringReturn.java
@@ -5,8 +5,20 @@ public class StringReturn {
     public static void main(String args[]){
         String message = greet();
         System.out.println(message);
+
+        Scanner input = new Scanner (System.in);
+        System.out.println("Enter your name: ");
+        String name = input.next();
+
+        String personalised = myGreet(name);
+//      String personalised = myGreet("Pallavi Kumari");
+        System.out.println(personalised);
     }
 
+    static String myGreet(String name){
+        String message = "Hello this is " + name;
+        return message;
+    }
 
 static String greet(){
     String greeting = "Hii!!!!, Good Evening";
