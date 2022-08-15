9f9f1805032cbd9ee65af5c2837c00251600d5c7
plpallavi <pallavi121202@gmail.com>
2022-08-15 18:22:14 +0530

    JAVA

---
+++ swap.java
@@ -0,0 +1,13 @@
+public class swap {
+    public static void main(String args[]){
+        int a = 40;
+        int b = 89;
+        // for swapping two numbers
+        int temp = a;
+        a = b;
+        b= temp;
+
+        System.out.println(a + " " + b);
+    }
+    
+}
