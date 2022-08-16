38553876d512e3e259a75690e49f94ac5162a99b
plpallavi <pallavi121202@gmail.com>
2022-08-16 23:12:30 +0530

    JAVA

--- swap.java
+++ swap.java
@@ -1,8 +1,9 @@
 public class swap {
+    // TO Swap two numbers
     public static void main(String args[]){
         int a = 40;
         int b = 89;
-        // for swapping two numbers
+        
         int temp = a;
         a = b;
         b= temp;
