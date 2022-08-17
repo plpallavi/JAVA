be485912ada11265189131479a7a40c9908f1f4b
plpallavi <pallavi121202@gmail.com>
2022-08-17 23:26:52 +0530

    JAVA

---
+++ passvalueexample.java
@@ -0,0 +1,10 @@
+public class passvalueexample {
+    public static void main(String args[]){
+        String name = "Pallavi";
+        greet(name);
+
+    }
+    static void greet(String naam){
+        System.out.println(naam);
+    }
+}
