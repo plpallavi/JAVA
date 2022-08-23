ea9b9e25607031cec239720686ad0ec80ab66b19
plpallavi <pallavi121202@gmail.com>
2022-08-23 21:03:48 +0530

    JAVA

---
+++ Arrays.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Arrays {
+    public static void main(String args[]){
+        //Syntax of Array
+        int[] numbers = {1,2,3,4,5};
+        System.out.println(numbers);
+
+        int[] rollno = new int[5];
+        
+        System.out.println(rollno);
+
+
+        int[] idno = new int[4];
+    }
+}
