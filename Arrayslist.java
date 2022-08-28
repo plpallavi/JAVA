213ccfa8421968d9c6f3c61cdb3503157c4d353d
plpallavi <pallavi121202@gmail.com>
2022-08-28 23:25:48 +0530

    JAVA

--- Arrayslist.java
+++ Arrayslist.java
@@ -1,5 +1,30 @@
 import java.util.*;
 
+class VOperation {
+    int[] arr;
+
+    void initilizer() {
+        arr = new int[] { 11, 6, 77, 8, 5, 44, 6, 9, 442, 86, 73, 49, 68, 82 };
+    }
+
+    void linearSearch() {
+        int flag = 0, temp = 0;
+        Scanner num = new Scanner(System.in);
+        int se = num.nextInt();
+        for (int i = 0; i < arr.length; i++) {
+            temp++;
+            if (arr[i] == se) {
+                flag = 1;
+                break;
+            }
+        }
+        if (flag == 1) {
+            System.out.println("element found at " + temp);
+        } else {
+            System.out.println("Element not found");
+        }
+
+    }
 public class Arrayslist {
     public static void main(String args[]) {
         // Syntax
