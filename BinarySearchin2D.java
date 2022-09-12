35aaf0048ab7f9419be3a80b2e37823b076c6305
plpallavi <pallavi121202@gmail.com>
2022-09-12 23:07:58 +0530

    JAVA

--- BinarySearchin2D.java
+++ BinarySearchin2D.java
@@ -3,9 +3,9 @@ import java.util.Arrays;
 class BinarySearchin2D {
     public static void main(String args[]) {
         int[][] num = {
-                { 1, 2, 3 }, // 0th index
-                { 4, 5, 6 }, // 1st index
-                { 7, 8, 9 } // c2nd index
+                { 41, 52, 63 },
+                { 44, 45, 46 },
+                { 47, 48, 49 }
         };
     }
 }
