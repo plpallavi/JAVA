3952d2c5b1e76a794b6d6edb23eb28fa8542ede7
plpallavi <pallavi121202@gmail.com>
2022-09-14 21:38:13 +0530

    JAVA

--- BinarySearchin2D.java
+++ BinarySearchin2D.java
@@ -2,10 +2,33 @@ import java.util.Arrays;
 
 class BinarySearchin2D {
     public static void main(String args[]) {
-        int[][] num = {
-                { 41, 52, 63 },
-                { 44, 45, 46 },
-                { 47, 48, 49 }
+        int[][] arr = {
+                { 10, 20, 30, 40 },
+                { 15, 25, 35, 45 },
+                { 28, 29, 37, 49 },
+                { 33, 34, 38, 50 }
         };
+        /*
+         * System.out.println(Arrays.toString(search(arr,49)));
+         * 
+         * static int[] search(int[][] matrix,int target) {
+         * int r = 0;
+         * int c = matrix.length- 1;
+         * 
+         * while(r < matrix.length && c>= 0){
+         * if(matrix[r][c] == target){
+         * return new int[]{r,c};
+         * }
+         * 
+         * if(matrix[r][c] < target){
+         * r++;
+         * }
+         * else{
+         * c--;
+         * }
+         * }
+         * }
+         * return new int[]{-1,-1};
+         */
     }
 }
---
+++ SortedMatrix.java
@@ -0,0 +1,19 @@
+public class SortedMatrix {
+    public static void main(String[] args) {
+
+    }
+    /*
+     * static int binarySearch(int[] matrix,int row,int cStart,int cend ){
+     * 
+     * }
+     * 
+     * static int[] search(int[][] matrix, int target) {
+     * int rows = matrix.length;
+     * int cols = matrix[0].length; // be cautious, matrix may be empty
+     * 
+     * if (rows == 1) {
+     * 
+     * }
+     * }
+     */
+}
