cf925df8ed9f24ab8cf6f6a9f2b2e4687d0ca4bd
plpallavi <pallavi121202@gmail.com>
2022-09-19 20:22:26 +0530

    JAVA

--- CyclicSort/NumsDissapearedInAnArray.java
+++ CyclicSort/NumsDissapearedInAnArray.java
@@ -1,3 +1,31 @@
 public class NumsDissapearedInAnArray {
+    public static void main(String[] args) {
 
+    }
+
+    public static int dissapeard(int[] arr) {
+        int i = 0;
+        while (i < arr.length) {
+            int correct = arr[i];
+            if (arr[i] < arr.length && arr[i] != arr[correct]) {
+                swap(arr, i, correct);
+            } else {
+                i++;
+            }
+        }
+
+        // Search for the missing number
+        for (int index = 0; index < arr.length; index++) {
+            if (arr[index] != index) {
+                return index;
+            }
+        }
+        return arr.length;
+    }
+
+    public static void swap(int[] arr, int first, int second) {
+        int temp = arr[first];
+        arr[first] = arr[second];
+        arr[second] = temp;
+    }
 }
