0b9d7ae9a187db11c7116576f8067a7ca8de6f45
plpallavi <pallavi121202@gmail.com>
2022-09-20 23:47:11 +0530

    JAVA

--- CyclicSort/NumsDissapearedInAnArray.java
+++ CyclicSort/NumsDissapearedInAnArray.java
@@ -1,6 +1,6 @@
 public class NumsDissapearedInAnArray {
     public static void main(String[] args) {
-
+        int[] arr = { 1, 3, 4, 5, 6, 7, 8, 9.0 };
     }
 
     public static int dissapeard(int[] arr) {
